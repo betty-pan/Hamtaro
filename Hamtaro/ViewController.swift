4499d11d0323c905ee3f2f5cdd1ccd53907a343a
BettyPan <betty19931218@gmail.com>
2020-11-13 18:56:44 +0800

    Initial Commit

---
+++ Hamtaro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hamtaro
+//
+//  Created by BettyPan on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
